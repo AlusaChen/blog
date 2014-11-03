4785660a2f23ba25e50bb265e26ae2cd6986001d
陈红 <chenhong@verycd.com>
2014-11-03 18:10:34 +0800

    cluster

--- app.new.js
+++ app.new.js
@@ -7,12 +7,13 @@ var express = require('express');
 var partials = require('express-partials');
 var routes = require('./routes');
 var http = require('http');
+var cluster = require('cluster');
+var os = require('os');
 var path = require('path');
 var MongoStore = require('connect-mongo')(express);
 var settings = require('./settings');
 var flash = require('connect-flash');
 var pjax = require('express-pjax');
-
 //handler error log
 
 var fs = require('fs');
@@ -43,43 +44,63 @@ app.use(express.bodyParser({ keepExtensions: true, uploadDir: path.join(__dirnam
 app.use(express.methodOverride());
 
 app.use(express.cookieParser());
+
 app.use(express.session({
 	secret : settings.cookieSecret,
 	key : settings.db,
 	cookie : {maxAge : 1000 * 60 * 60 * 24 * 30},
 	store : new MongoStore({
 		db : settings.db
+	}, function(e){
+		app.use(app.router);
+		app.use(express.static(path.join(__dirname, 'public')));
+
+
+		//log error
+		app.use(function(err, req, res, next) {
+			var meta = '[' + new Date() + '] ' + req.url + '\n';
+			errorLog.write(meta + err.stack + '\n');
+			next();
+		});
+
+
+
+		app.use(function(err, req, res, next) {
+			if(!err) return next(); // you also need this line
+		    res.render("500");
+		});
+
+
+		// development only
+		if ('development' == app.get('env')) {
+		  app.use(express.errorHandler());
+		}
+
+		routes(app);
+
+		if(cluster.isMaster)
+		{
+			for(var i = 0; i < os.cpus().length; i++) 
+			{
+				console.log("cluster: " + (i + 1) + " start");
+		        cluster.fork();
+			}
+
+			cluster.on('listening', function(worker, address){
+				console.log('[master] ' + 'listening: worker' + worker.id + ',pid:' + worker.process.pid + ', Address:' + address.address + ":" + address.port);
+			});
+
+			cluster.on('exit', function(worker, code, signal) {
+				console.log('worker ' + worker.process.pid + ' died');
+			});
+		}
+		else
+		{
+			console.log('[worker] ' + "start worker ..." + cluster.worker.id);
+			http.createServer(app).listen(app.get('port'), function(){
+				console.log('Express server listening on port ' + app.get('port'));
+			});
+		}
 	})
 }));
 
-
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-//log error
-app.use(function(err, req, res, next) {
-	var meta = '[' + new Date() + '] ' + req.url + '\n';
-	errorLog.write(meta + err.stack + '\n');
-	next();
-});
-
-
-
-app.use(function(err, req, res, next) {
-	if(!err) return next(); // you also need this line
-    res.render("500");
-});
-
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-routes(app);
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
